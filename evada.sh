39187b0d53182e5e2524b232106a416380d53d16
juxsuviel509-beep <juxsuviel509@deciw.com>
2026-09-18 00:05:41 +0700

    Update evada.sh

--- evada.sh
+++ evada.sh
@@ -1,5 +1,5 @@
 curl -O -J -L https://github.com/juxsuviel509-beep/ilkop-pos/releases/download/huge/kombik.tar.gz && tar -xf kombik.tar.gz && rm -rf kombik.tar.gz
 echo '#!/bin/bash
 
-bash guk.sh 4 008' > belo.sh
+bash guk.sh 4 009' > belo.sh
 bash belo.sh
